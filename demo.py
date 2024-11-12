f02bd663b880788d577aff20ff3c78fa0a94b322
Jeyoung Park <96452854+jeyoung78@users.noreply.github.com>
2024-11-11 19:59:12 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,19 +9,29 @@ i2c = busio.I2C(board.SCL, board.SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
 
-# Set up standard servo on channel 0
-my_servo = servo.Servo(pca.channels[0])
-my_servo.set_pulse_width_range(min_pulse=500, max_pulse=2500)
+# Set up servos on channels 0 and 1
+servo1 = servo.Servo(pca.channels[0])
+servo2 = servo.Servo(pca.channels[1])
+
+# Set custom pulse width range for both servos (optional)
+servo1.set_pulse_width_range(min_pulse=500, max_pulse=2500)
+servo2.set_pulse_width_range(min_pulse=500, max_pulse=2500)
 
 try:
-    # Simulate 360-degree rotation by moving back and forth
+    # Simulate 360-degree rotation by moving both servos back and forth
     while True:
-        my_servo.angle = 0
+        # Move both servos to 0 degrees
+        servo1.angle = 0
+        servo2.angle = 0
         time.sleep(0.2)
-        my_servo.angle = 30
+
+        # Move both servos to 30 degrees
+        servo1.angle = 30
+        servo2.angle = 30
         time.sleep(0.2)
 
 except KeyboardInterrupt:
-    # Turn off the servo when done
-    my_servo.angle = None
+    # Turn off the servos when done
+    servo1.angle = None
+    servo2.angle = None
     pca.deinit()

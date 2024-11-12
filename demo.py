6086034becb0866cd8f2dd5df5358c1ad52d21c8
Jeyoung Park <96452854+jeyoung78@users.noreply.github.com>
2024-11-11 20:01:16 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -29,14 +29,14 @@ try:
         servo2.angle = 0
         servo3.angle = 0
         servo4.angle = 0
-        time.sleep(0.2)
+        time.sleep(0.65)
 
         # Move both servos to 30 degrees
-        servo1.angle = 30
-        servo2.angle = 30
-        servo3.angle = 30
-        servo4.angle = 30
-        time.sleep(0.2)
+        servo1.angle = 180
+        servo2.angle = 180
+        servo3.angle = 180
+        servo4.angle = 180
+        time.sleep(0.65)
 
 except KeyboardInterrupt:
     # Turn off the servos when done

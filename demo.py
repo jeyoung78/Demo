3bde7f69cf280bbc4a38185b5c71cf5aaa12ec78
Jeyoung Park <96452854+jeyoung78@users.noreply.github.com>
2024-11-11 19:49:36 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -17,9 +17,9 @@ try:
     # Simulate 360-degree rotation by moving back and forth
     while True:
         my_servo.angle = 0
-        time.sleep(5)
+        time.sleep(0.7)
         my_servo.angle = 180
-        time.sleep(5)
+        time.sleep(0.7)
 
 except KeyboardInterrupt:
     # Turn off the servo when done
